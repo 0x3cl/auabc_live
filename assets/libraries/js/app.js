58f0a1c8c8b366db0ff7c260a2c0b70edebd25b9
Carl Llemos <87264083+iamcarlllemos@users.noreply.github.com>
2023-08-08 08:49:31 +0800

    Update app.js

--- assets/libraries/js/app.js
+++ assets/libraries/js/app.js
@@ -1,6 +1,57 @@
-// INITIALIZE AOS
+// START LOADER
+startLoader();
 
+// EXIT LOADER AFTER 3000 MILLISECONDS
+setTimeout(exitLoader, 3000);
+
+// INITIALIZE AOS
 AOS.init();
+
+function startLoader() {
+    const loaders = ['.loader-primary', '.loader-secondary', '.loader-tertiary'];
+    let delay = 200;
+
+    loaders.reduce((promise, loader) => {
+        return promise.then(() => {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    $(loader).css('left', '0');
+                    resolve();
+                }, delay);
+            });
+        });
+    }, Promise.resolve())
+    .then(() => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                $('.loader-content').css('left', '0');
+                resolve();
+            }, delay);
+        });
+    })
+    .then(() => {
+        $('.loader-content .logo-content').fadeIn(500);
+        $('.loader').css('background-color', 'transparent');
+    });
+}
+
+function exitLoader() {
+    $('.loader-content .logo-content').fadeOut(500, () => {
+        const loaders = ['.loader-tertiary', '.loader-secondary', '.loader-primary'];
+        let delay = 200;
+
+        loaders.reduce((promise, loader) => {
+            return promise.then(() => {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        $(loader).css('left', '100%');
+                        resolve();
+                    }, delay);
+                });
+            });
+        }, Promise.resolve());
+    });
+}
 
 // SHOW HEADER WHEN WINDOW SCROLL
 
